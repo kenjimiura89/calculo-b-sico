2276b1b705850a9d0b67d235345ba64d7b6b73fc
kenjimiura89 <paulokmiura@gmail.com>
2022-10-24 18:07:52 -0300

    Atividade sobre cálculo básico

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Atividade:
+Crie um programa que receba dois números inteiros e depois mostre o resultado da soma e da multiplicação, de um número pelo outro.
---
+++ calculo.py
@@ -0,0 +1,8 @@
+'''
+Crie um programa que receba dois números inteiros e depois mostre o resultado da soma e da multiplicação, de um número pelo outro.
+'''
+numero1 = int(input("informe um número"))
+numero2 = int(input("informe outro número"))
+resultado_soma = numero1 + numero2
+resultado_multiplicacao = numero1 * numero2 
+print("a soma foi", resultado_soma, "e a multiplicação foi", resultado_multiplicacao )
